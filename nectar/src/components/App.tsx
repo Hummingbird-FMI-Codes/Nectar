1d403f1d84b8f0e606bd901344386550db76acec
Mihail Tsankov <mihail.tsankov@trading212.com>
2025-03-15 16:07:44 +0200

    added json

--- nectar/src/components/App.tsx
+++ nectar/src/components/App.tsx
@@ -2,19 +2,17 @@ import { UploadPhoto } from "./UploadPhoto/UploadPhoto";
 
 function App() {
   return (
-    <>
-      <div
-        style={{
-          backgroundColor: "lightblue",
-          display: "flex",
-          flexGrow: 1,
-          justifyContent: "center",
-          alignItems: "center",
-        }}
-      >
-        <UploadPhoto />
-      </div>
-    </>
+    <div
+      style={{
+        backgroundColor: "lightblue",
+        display: "flex",
+        flexGrow: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <UploadPhoto />
+    </div>
   );
 }
 
